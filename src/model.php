012254ed32cacd5c54f7e15ec107c83eb5bb48c3
jeremy-mls <contact@jeremy_monlouis.fr>
2022-06-22 23:21:50 +0200

    Gérez les erreurs

--- src/model.php
+++ src/model.php
@@ -43,8 +43,5 @@ function dbConnect()
     try {
         $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
 
-        return $database;
-    } catch(Exception $e) {
-        die('Erreur : '.$e->getMessage());
-    }
+    return $database;
 }
