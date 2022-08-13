557ea0c0ec00361a288d29cdb0be42e876ac7293
jeremy-mls <contact@jeremy_monlouis.fr>
2022-08-14 00:13:13 +0200

    google analytics

--- index.php
+++ index.php
@@ -5,3 +5,13 @@ if(! isset($_SESSION)){
 require_once 'vendor/autoload.php';
 define('ROOT', __DIR__);
 require_once 'src/config/routes.php';
+?>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BZ4S14M78H"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-BZ4S14M78H');
+</script>
