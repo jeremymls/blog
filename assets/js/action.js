7008b6a5984659b255f1c4faece43bd43998a157
jeremy-mls <contact@jeremy_monlouis.fr>
2022-08-16 22:07:56 +0200

    Restrict the size of profile images

--- assets/js/action.js
+++ assets/js/action.js
@@ -1,13 +1,27 @@
+const thumbnail = document.getElementById("thumbnailOverview");
+const imgSize = document.getElementById("imgSize");
+
 document.getElementById("picture").onchange = function () {
-
+    imgSize.innerHTML = "Taille du fichier: " + formatBytes(this.files[0].size);
+    if (this.files[0].size > 512000) {
+        alert("Le fichier est trop gros");
+        this.value = "";
+        thumbnail.src = "/assets/img/profile.png";
+    }
     var reader = new FileReader();
-
     reader.onload = function (e) {
-
         // get loaded data and render thumbnail.
-        document.getElementById("thumbnailOverview").src = e.target.result;
+        thumbnail.src = e.target.result;
     };
-
     // read the image file as a data URL.
     reader.readAsDataURL(this.files[0]);
-};
+}
+
+function formatBytes(size, decimals = 2) {
+    if (size === 0) return '0 bytes';
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(size) / Math.log(k));
+    return parseFloat((size / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
