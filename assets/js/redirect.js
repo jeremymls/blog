a88942ded3c5f4ada4a00213a5870abc5c542482
jeremy-mls <contact@jeremy_monlouis.fr>
2022-10-18 12:19:49 +0200

    HOTFIX - redirect

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -4,21 +4,14 @@ function countDown() {
         count--; 
         timer.innerHTML = "Vous serez redirigé·e dans <strong>" + count + "</strong> secondes."; // Timer Message
         setTimeout("countDown()", 1000);
-        //count == 0 ? setTimeout("countDown()", 3800):setTimeout("countDown()", 1000);
-
     } else {
         window.location.href = redirect;
     }
 }
-
 countDown();
-if (window.innerWidth > 768) {
-    scrollTo(0, 400);
-}
 
-createPageTransition();
-//setInterval(createPageTransition, 3000);
-
+// createPageTransition();
+setInterval(createPageTransition, 3000);
 function createPageTransition() {
     const el = document.createElement('div');
     el.className = 'page-transition';
@@ -30,9 +23,3 @@ function createPageTransition() {
         el.className = 'page-transition page-transition--in';
     }, 100);
 }
-
-// écouteur d'évènement sur le defilement de la page
-// window.addEventListener('scroll', function() {
-//     var scroll = window.pageYOffset;
-//     console.log(scroll);
-// });
